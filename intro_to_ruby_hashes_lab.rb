38f0914246d4563beeb6fede27ae682fd9d28fa5
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-07 03:00:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,25 +1,25 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash{"my name" => "Yimei", "my dog" => "Virgil"} 
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {:name => "Grace Hopper"} 
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_generator = {:id => 22} 
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+hash = {key => value} 
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+hash[key]
 end
 
 def update_counting_hash(hash, key)
